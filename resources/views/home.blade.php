184f3f3213fe108035834e096c8195cb8860a38e
Ilaria Salvati <ilariasalvati00@gmail.com>
2024-01-23 20:26:35 +0100

    esercizio completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Dati da config/data.js: {{ $home }}</h1>
-    <p>This is my paragraph content.</p>
+
+    <div class="container">
+    @foreach ($movies as $movie)
+        <div class="card">
+            <h1>{{ $movie->title }}</h1>
+            <h4>{{ $movie->original_title }}</h4>
+            <span>Nationality: {{ $movie->nationality }}</span>
+            <span>Date: {{ $movie->date }}</span>
+            <span>Vote: {{ $movie->vote }}</span>
+        </div>
+        
+    @endforeach
+    </div>
 @endsection
